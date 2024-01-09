2b23b05bceb6e207cf390052c19ab1199563535d
timone019 <143154960+timone019@users.noreply.github.com>
2024-01-09 10:38:37 -0800

    added passport get movies endmpoint

--- index.js
+++ index.js
@@ -146,7 +146,8 @@ app.post(
 );
 
 // Read: Return a list of ALL movies
-app.get("/movies", async (req, res) => {
+app.get("/movies", passport.authenticate("jwt", { session: false }),
+async (req, res) => {
   await Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
